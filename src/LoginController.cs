ff7d0ff3e713a25239992dffec4d1342ada13b75
Cjreynol <cjreynol13@aol.com>
2014-11-23 18:10:11 -0500

    Added initial batch of source code files for controllers.

---
+++ src/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class LoginController
+    {
+        private DBConnector dbconn;
+        private LoginForm loginForm;
+
+        public LoginController(DBConnector dbc)
+        {
+            this.dbc = dbc;
+            loginForm = new LoginForm(this);
+        }
+
+        public void Submit(string uname, string pword)
+        {
+            if (VerifyUser(pword, dbc.GetUser(uname).Password))
+            {
+                loginForm.Destroy();
+                new MainController(dbconn, uname);
+            }
+            else
+                loginForm.DisplayMessage(errorMessage);
+        }
+    }
+}
---
+++ src/SelectEmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class SelectEmployeeController
+    {
+        private DBConnector dbc;
+        private MainController mcp;
+        private SelectEmployeeForm seform;
+
+        public SelectEmployeeController(DBConnector dbc, MainController mcp)
+        {
+            this.dbc = dbc;
+            this.mcp = mcp;
+        }
+
+        public void Start()
+        {
+            seform = new SelectEmployeeForm(this);
+            seform.Populate(dbc.GetEmployees());
+        }
+
+        public void Submit(User user)
+        {
+            seform.Destroy();
+            mcp.StartAlterAvailability(user);
+        }
+
+    }
+}
